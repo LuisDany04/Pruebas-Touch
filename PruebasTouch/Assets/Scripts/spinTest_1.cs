1bbae0794ddcc67334d7f5808cb0d509d2770b9d
Daniel Gonzalez <dany_slender04@hotmail.com>
2020-10-08 20:57:18 -0500

    2 scripts added

---
+++ PruebasTouch/Assets/Scripts/spinTest_1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinTest_1 : MonoBehaviour {
+    // Start is called before the first frame update
+    float f_lastX = 0.0f;
+    float f_difX = 0.5f;
+    float f_steps = 0.0f;
+    int i_direction = 1;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            f_difX = 0.0f;
+        }
+        else if (Input.GetMouseButton(0)) {
+            f_difX = Mathf.Abs(f_lastX - Input.GetAxis("Mouse X"));
+
+            if (f_lastX < Input.GetAxis("Mouse X")) {
+                i_direction = -1;
+                transform.Rotate(Vector3.down, -f_difX);
+                //transform.rotation = Quaternion.Euler(0f, 0f, -f_difX);
+            }
+            if (f_lastX > Input.GetAxis("Mouse X")) {
+                i_direction = 1;
+                transform.Rotate(Vector3.down, f_difX);
+                //transform.rotation = Quaternion.Euler(0f, 0f, -f_difX);
+            }
+
+            f_lastX = -Input.GetAxis("Mouse X");
+        }
+        else {
+            if (f_difX > 0.5f) f_difX -= 0.05f;
+            if (f_difX < 0.5f) f_difX += 0.05f;
+
+            transform.Rotate(Vector3.down, f_difX * i_direction);
+        }
+    }
+}
